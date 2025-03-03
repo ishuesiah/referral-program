a6bd204fa67c0928bab334dd98224a06a984eea5
ishuesiah <129911713+ishuesiah@users.noreply.github.com>
2025-03-03 15:43:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
+const crypto = require('crypto');
 const app = express();
 
 // Define your referral code generator function here
